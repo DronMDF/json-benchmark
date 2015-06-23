c849c9a1f0ba4d12be84bcffd3b8a7063cc5a875
Andrey Valyaev <dron.valyaev@gmail.com>
2015-06-23 14:33:29 +0300

    Вынес имплементацию BoostPTree в отдельный файл

---
+++ BoostPTree.h
@@ -0,0 +1,16 @@
+
+#pragma once
+#include <string>
+#include <sstream>
+#include <boost/property_tree/json_parser.hpp>
+
+class BoostPTree {
+public:
+	void parse(const std::string &json) {
+		std::istringstream jfile(json);
+		boost::property_tree::read_json(jfile, root);
+	}
+
+private:
+	boost::property_tree::ptree root;
+};
--- json-benchmark.cpp
+++ json-benchmark.cpp
@@ -1,46 +1,32 @@
 
 #include <string>
 #include <fstream>
-#include <boost/property_tree/json_parser.hpp>
 #include <celero/Celero.h>
+
+#include "BoostPTree.h"
 
 using namespace std;
 
-class NullJson {
-public:
-	void parse(const string &) {}
-};
-
-class BoostPTree {
-public:
-	void parse(const string &json) {
-		istringstream jfile(json);
-		boost::property_tree::read_json(jfile, root);
-	}
-
-private:
-	boost::property_tree::ptree root;
-};
-
 template<typename Impl>
-class ParseFixture : public celero::TestFixture, public Impl {
+class ParseFixture : public celero::TestFixture {
+private:
+	Impl impl;
+	string json;
 public:
 	virtual void setUp(int64_t) {
 		ifstream t("data.json");
 		json.assign(istreambuf_iterator<char>(t), istreambuf_iterator<char>());
 	}
 
-	string json;
+	void parse() {
+		impl.parse(json);
+	}
+
 };
 
-BASELINE_F(Parsing, NullJson, ParseFixture<NullJson>, 3, 1)
+BASELINE_F(Parsing, BoostPTree, ParseFixture<BoostPTree>, 10, 1)
 {
-	parse(json);
-}
-
-BENCHMARK_F(Parsing, BoostPTree, ParseFixture<BoostPTree>, 3, 1)
-{
-	parse(json);
+	parse();
 }
 
 CELERO_MAIN
